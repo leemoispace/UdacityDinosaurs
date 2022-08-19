6dc0f263a18e8dc11bf0b556c5f8d8d68e0bde26
leemoispace <>
2022-08-19 22:51:57 +0400

    set up unit change part html elements and function.

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
+// Create Dino Constructor
 
-    // Create Dino Constructor
+// import myJson from "./dino.json" assert { type: "json" };
+import { myJson } from "./dino.json";
 
+// let dinos = JSON.parse(JSONfile);
+console.log(myJson);
 
-    // Create Dino Objects
+// Create Dino Objects
 
+// Create Human Object
 
-    // Create Human Object
+// Use IIFE to get human data from form
 
-    // Use IIFE to get human data from form
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Generate Tiles for each Dino in Array
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Add tiles to DOM
 
-
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic
+
+// Change unit between metric and imperial for html display
+function unitsChange() {
+  if (document.getElementById("metric").checked) {
+    document.getElementById("metric-form").style.display = "block";
+    document.getElementById("imperial-form").style.display = "none";
+  } else {
+    document.getElementById("metric-form").style.display = "none";
+    document.getElementById("imperial-form").style.display = "block";
+  }
+}
