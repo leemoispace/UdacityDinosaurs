91c1e43f57898be3655dc1e10996d31b0fdde15c
leemoispace <>
2022-08-20 22:11:41 +0400

    make dinosaur and human objects and the compare methods.

--- app.js
+++ app.js
@@ -1,25 +1,178 @@
-// Create Dino Constructor
+//dino datas array
+const rawDinoData = [
+  {
+    species: "Triceratops",
+    weight: 13000,
+    height: 114,
+    diet: "herbivore",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "First discovered in 1889 by Othniel Charles Marsh",
+  },
+  {
+    species: "Tyrannosaurus Rex",
+    weight: 11905,
+    height: 144,
+    diet: "carnivore",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "The largest known skull measures in at 5 feet long.",
+  },
+  {
+    species: "Anklyosaurus",
+    weight: 10500,
+    height: 55,
+    diet: "herbivore",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Anklyosaurus survived for approximately 135 million years.",
+  },
+  {
+    species: "Brachiosaurus",
+    weight: 70000,
+    height: "372",
+    diet: "herbivore",
+    where: "North America",
+    when: "Late Jurassic",
+    fact: "An asteroid was named 9954 Brachiosaurus in 1991.",
+  },
+  {
+    species: "Stegosaurus",
+    weight: 11600,
+    height: 79,
+    diet: "herbivore",
+    where: "North America, Europe, Asia",
+    when: "Late Jurassic to Early Cretaceous",
+    fact: "The Stegosaurus had between 17 and 22 seperate plates and flat spines.",
+  },
+  {
+    species: "Elasmosaurus",
+    weight: 16000,
+    height: 59,
+    diet: "carnivore",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Elasmosaurus was a marine reptile first discovered in Kansas.",
+  },
+  {
+    species: "Pteranodon",
+    weight: 44,
+    height: 20,
+    diet: "carnivore",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Actually a flying reptile, the Pteranodon is not a dinosaur.",
+  },
+  {
+    species: "Pigeon",
+    weight: 0.5,
+    height: 9,
+    diet: "herbivore",
+    where: "Worldwide",
+    when: "Holocene",
+    fact: "All birds are living dinosaurs.",
+  },
+];
 
-// import myJson from "./dino.json" assert { type: "json" };
-import { myJson } from "./dino.json";
+// Create Dino Constructor with data
+function DinoConstructor(dinoData, units) {
+  this.species = dinoData.species;
+  this.diet = dinoData.diet;
+  this.where = dinoData.where;
+  this.when = dinoData.when;
+  this.fact = dinoData.fact;
+  if (units === "metric") {
+    this.weight = Math.round(dinoData.weight / 2.21);
+    this.height = Math.round(dinoData.height * 2.54);
+  } else {
+    this.weight = dinoData.weight;
+    this.height = dinoData.height;
+  }
+}
 
-// let dinos = JSON.parse(JSONfile);
-console.log(myJson);
+// prototype dinosaur with methods
+const protoDino = {
+  // Create Dino Compare Method 1 for weight
+  compareWeight: function (humanWeight) {
+    const weightRatio = (this.weight / humanWeight).toFixed(1);
+    if (weightRatio > 1) {
+      return `${this.species} weighed ${(this.weight / humanWeight).toFixed(
+        1
+      )} times more than you!`;
+    }
+    if (weightRatio < 1) {
+      return `You weigh ${(humanWeight / this.weight).toFixed(
+        1
+      )} times more than ${this.species}!`;
+    }
+    return `You weigh the same as ${this.species}!`;
+  },
+  // Create Dino Compare Method 2 for height
+  compareHeight: function (humanHeight) {
+    const heightRatio = (this.height / humanHeight).toFixed(1);
+    if (heightRatio > 1) {
+      return `${this.species} was ${(this.height / humanHeight).toFixed(
+        1
+      )} times taller than you!`;
+    }
+    if (heightRatio < 1) {
+      return `You are ${(humanHeight / this.height).toFixed(
+        1
+      )} times taller than ${this.species}!`;
+    }
+    return `You are the same height as ${this.species}!`;
+  },
+  // Create Dino Compare Method 3 for diet
+  compareDiet: function (humanDiet) {
+    const article = humanDiet === "omnivore" ? "an" : "a";
+    if (humanDiet === this.diet) {
+      return `You are ${article} ${humanDiet} and ${this.species} was too!`;
+    } else {
+      return `You are ${article} ${humanDiet}, but ${this.species} was a ${this.diet}.`;
+    }
+  },
+};
 
-// Create Dino Objects
+//assign the prototype to the constructor
+DinoConstructor.prototype = protoDino;
 
-// Create Human Object
+// Create Dino Object arrays
+function createDinoArray(units) {
+  const dinos = rawDinoData;
+  const dinoArray = [];
+  dinos.forEach(function (dino) {
+    dinoArray.push(new DinoConstructor(dino, units));
+  });
+  // Insert the human placeholder here so that Human should be in the centre in the grid elements
+  dinoArray.splice(4, 0, "human placeholder");
+  return dinoArray;
+}
+// Create Human Object using data from form
+function getHumanData() {
+  let height, weight, units;
 
-// Use IIFE to get human data from form
+  if (document.getElementById("metric").checked) {
+    height = document.getElementById("height-metric").value;
+    weight = document.getElementById("weight-metric").value;
+    units = "metric";
+  } else {
+    height =
+      document.getElementById("feet").value * 12 +
+      Number(document.getElementById("inches").value);
+    weight = document.getElementById("weight-imperial").value;
+    units = "imperial";
+  }
 
-// Create Dino Compare Method 1
-// NOTE: Weight in JSON file is in lbs, height in inches.
+  const humanData = {
+    name: document.getElementById("name").value,
+    height: height,
+    weight: weight,
+    diet: document.getElementById("diet").value,
+    units: units,
+  };
 
-// Create Dino Compare Method 2
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-// Create Dino Compare Method 3
-// NOTE: Weight in JSON file is in lbs, height in inches.
+  return humanData;
+}
 
 // Generate Tiles for each Dino in Array
 
